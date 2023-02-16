4f0782eced079b06e9cc50a5cbeae74eb33e6607
Biademery <biademery@gmail.com>
2023-02-16 13:26:53 -0300

    changes

--- www/src/http-common.js
+++ www/src/http-common.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 // axios.defaults.headers.common['Access-Control-Allow-Headers'] = '*';
 
 const api = axios.create({
-  baseURL: 'https://still-anchorage-45008.herokuapp.com/',
+  baseURL: 'http://127.0.0.1:8000/',
   headers: {
     'Content-type': 'application/json',
   },
