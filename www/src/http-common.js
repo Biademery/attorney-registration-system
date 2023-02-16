fded67ccb1a5a1d5bdb594a94438a78826632075
Biademery <biademery@gmail.com>
2023-02-16 13:36:32 -0300

    changes

--- www/src/http-common.js
+++ www/src/http-common.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 // axios.defaults.headers.common['Access-Control-Allow-Headers'] = '*';
 
 const api = axios.create({
-  baseURL: 'https://git.heroku.com/peaceful-sands-97197.git/',
+  baseURL: 'http://127.0.0.1:8000/',
   headers: {
     'Content-type': 'application/json',
   },
