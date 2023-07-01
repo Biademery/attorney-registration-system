99d21810d0bf725e643d1ed16442a856d5255488
Bia D'Emery <biademery@gmail.com>
2023-07-01 11:25:44 -0300

    add comment

--- www/src/http-common.js
+++ www/src/http-common.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
+  // api está desconectada no momento
   baseURL: 'https://intense-wildwood-47079-cfbddbbcaa51.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
